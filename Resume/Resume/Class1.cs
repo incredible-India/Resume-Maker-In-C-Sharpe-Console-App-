a8135b9be914b967b75392abe5ec684a1a8d8f87
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-02-16 19:24:02 +0530

    added userinformation class

---
+++ Resume/Resume/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInformation
+{
+    class User
+    {
+        private String fname;
+        private String mname;
+        private String lname;
+        private String address;
+        private String phone;
+        private String college;
+        private String skills;
+        private String email;
+
+
+        private  void initiliseValueToTheUser(String fname,String mname,String lname,String address,String phone,
+                                                String college,String skills,String email)
+        {
+            this.fname = fname;
+            this.mname = mname;
+            this.lname = lname;
+            this.address = address;
+            this.phone = phone;
+            this.college = college;
+            this.skills = skills;
+            this.email = email;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Resume/Resume/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
